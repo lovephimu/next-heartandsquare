ff0edcbf61a2b13061b951280a7a08d225b00f48
lovephimu <soe.nlb@gmail.com>
2023-12-17 23:29:55 +0100

    Add base layout

--- app/page.tsx
+++ app/page.tsx
@@ -1,113 +1,68 @@
-import Image from 'next/image'
+import Image from 'next/image';
 
 export default function Home() {
+  async function getBlogEntries() {
+    const baseUrl = process.env.baseUrl;
+    try {
+      const response = await fetch(`${baseUrl}/api/collections/blog/entries`);
+
+      const data = await response.json();
+      if (response.ok) {
+        console.log(data);
+      } else {
+        console.log('fetch didnt work out');
+      }
+    } catch (error) {
+      console.log('Fetch didnt work');
+    }
+  }
+  getBlogEntries();
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+    <main className="h-screen">
+      {/* <!-- hero --> */}
+      <div className="w-full flex flex-col h-4/5 justify-center items-center">
+        {/* <!-- {{ svg src="/logo/hands.svg" }} --> */}
+        <Image
+          alt="heart and square logo"
+          className="w-32 h-auto"
+          height={128}
+          width={128}
+          src="/images/hands.svg"
+        />
+        {/* <!-- site name logo --> */}
+        <div className="flex justify-center w-full">
+          <div className="w-1/3 text-right pr-0.5">
+            <h1>heart</h1>
+          </div>
+          <div className="w-auto text-center">
+            <h1>&</h1>
+          </div>
+          <div className="w-1/3 text-left pl-0.5">
+            <h1>square</h1>
+          </div>
+        </div>
+        {/* <!-- credit --> */}
+        <div className="flex w-full">
+          <div className="w-1/2"></div>
+          <div className="w-1/2 text-left">
+            <h4>by</h4>
+            <h2>lovephimu</h2>
+          </div>
+        </div>
+        {/* <!-- content --> */}
+        <div className="flex w-full">
+          <div className="w-1/2 text-right">
+            <h5>Programming</h5>
+            <h5>Architecture</h5>
+            <h5>Illustration</h5>
+          </div>
+          <div className="w-1/2 text-left"></div>
         </div>
       </div>
-
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore starter templates for Next.js.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
+      {/* <!-- main content --> */}
+      <div className="bg-contrastBlue mx-auto px-2 min-h-[20%] flex flex-col items-center justify-center">
+        <h1>Test</h1>
       </div>
     </main>
-  )
+  );
 }
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,4 +1,4 @@
-import type { Config } from 'tailwindcss'
+import type { Config } from 'tailwindcss';
 
 const config: Config = {
   content: [
@@ -8,13 +8,26 @@ const config: Config = {
   ],
   theme: {
     extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+      colors: {
+        mainBlue: '#6BF1F0',
+        transitionBlue: '#30B2DC',
+        contrastBlue: '#30B2DC',
+        textBlue: '#1D4775',
+        textYellow: '#F9E000',
+      },
+      fontFamily: {
+        'roboto-slab-100': ['Roboto Slab Thin', 'sans-serif'],
+        'roboto-slab-200': ['Roboto Slab ExtraLight', 'sans-serif'],
+        'roboto-slab-300': ['Roboto Slab Light', 'sans-serif'],
+        'roboto-slab-400': ['Roboto Slab Regular', 'sans-serif'],
+        'roboto-slab-500': ['Roboto Slab Medium', 'sans-serif'],
+        'roboto-slab-600': ['Roboto Slab SemiBold', 'sans-serif'],
+        'roboto-slab-700': ['Roboto Slab Bold', 'sans-serif'],
+        'roboto-slab-800': ['Roboto Slab ExtraBold', 'sans-serif'],
+        'roboto-slab-900': ['Roboto Slab Black', 'sans-serif'],
       },
     },
   },
   plugins: [],
-}
-export default config
+};
+export default config;
